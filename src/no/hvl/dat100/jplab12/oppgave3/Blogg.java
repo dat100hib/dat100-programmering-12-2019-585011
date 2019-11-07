641acb4331b8642bc159ecb38a1b3d796eb46a6a
585011 <54099824+585011@users.noreply.github.com>
2019-11-07 13:11:47 +0100

    alt oblig ferdig.

--- src/no/hvl/dat100/jplab12/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab12/oppgave3/Blogg.java
@@ -6,67 +6,167 @@ import no.hvl.dat100.jplab12.oppgave1.*;
 public class Blogg {
 
 	// TODO: objektvariable 
+	private Innlegg[] tabell;
+	protected int nesteledige = 0;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.tabell = new Innlegg[20];
+		this.nesteledige = 0;
+		//throw new UnsupportedOperationException(TODO.constructor("Blogg"));
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.tabell = new Innlegg[lengde];
+		nesteledige = 0;
+		//throw new UnsupportedOperationException(TODO.constructor("Blogg"));
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		int counter = 0;
+		for (int i = 0; i < tabell.length; i++) {
+			if(tabell[i] != null) {
+				counter++;
+			}
+		}
+		//throw new UnsupportedOperationException(TODO.method());
+		return counter;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.tabell;
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
+		
+		int a = -1;
+		int pos = 0;
+		while (pos < nesteledige && a == -1) {
+			if(tabell[pos].erLik(innlegg)) {
+				a = pos;
+			}
+			pos++;
+		}		
+		return a;
+		
 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		boolean funnet = false;
+		
+		for(int z = 0; z < tabell.length; z++) {
+			if(tabell[z] != null) {
+				if(tabell[z].erLik(innlegg)) {
+					funnet = true;
+				}
+			}
+		}
+		
+			
+		return funnet;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+		boolean ledig = false;
+		for(int i = 0; i < tabell.length; i++) {
+			if(tabell[i] == null && i < tabell.length ) {
+				ledig = true;
+				
+			
+			}
+		}
+		return ledig;
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean sattinn = false;
+		//System.out.println(innlegg.getBruker());
+		if(ledigPlass() && !finnes(innlegg)) {
+			tabell[nesteledige] = innlegg;
+			nesteledige++;
+			sattinn = true;
+		}else {
+			return false;
+		}
+		return sattinn;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg t = null;
+		Innlegg a = null;
+		for(int i = 0; i < tabell.length-1; i++) {
+			t = tabell[i+1];
+			a = tabell[i];
+		}
+		return getAntall() +"\n"+ a.toString() + t.toString();
+				
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	// valgfrie oppgaver nedenfor
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] ny = new Innlegg[2*tabell.length];
+		for(int i = 0; i < nesteledige; i++) {
+			ny[i] = tabell[i];
+		}
+		tabell = ny;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean lagttil = false;
+		
+		if(!finnes(innlegg) && ledigPlass()) {
+			leggTil(innlegg);
+			lagttil = true;
+		}else if(finnes(innlegg)) {
+			return lagttil;
+			
+		} else if(!finnes(innlegg) && !ledigPlass()) {
+			utvid();
+			leggTil(innlegg);
+			lagttil = true;
+		}
+		return lagttil;
+		//throw new UnsupportedOperationException(TODO.method());
 		
 	}
 	
 	public void slett(Innlegg innlegg) {
+		int p = finnInnlegg(innlegg);
+		boolean slettet = false;
+		if (p>=0) {
+			nesteledige--;
+			tabell[p] = tabell[nesteledige];
+			tabell[nesteledige] = null;
+			slettet = true;
+		}else {
+			slettet = false;
+		}
 		
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	public int[] search(String keyword) {
+		//feil, fann ikkje ut riktig metode
+		int[] id1 = new int[tabell.length];
+		for(int i = 0; i < tabell.length; i++) {
+			if(keyword.equals(i)){
+				System.out.println(tabell[i]);
+				
+			}
 		
-		throw new UnsupportedOperationException(TODO.method());
+		}
+		return id1;
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 }
--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -1,17 +1,52 @@
 package no.hvl.dat100.jplab12.oppgave4;
 
+import java.io.BufferedWriter;
+
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab12.oppgave3.*;
 import no.hvl.dat100.jplab12.common.TODO;
+import no.hvl.dat100.jplab12.oppgave1.Innlegg;
 
 public class SkrivBlogg {
 
 	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
 
 	public static boolean skriv(Blogg samling, String filnavn) {
+		boolean skrevet = false;
+		System.out.println(skrevet);
+		
+		
+		//PrintWriter skriver = null;
+		Innlegg[] sm = samling.getSamling();
+		int an = samling.getAntall();
+		Innlegg a = null;
+		Innlegg b = null;
+		
+		try {
+			
+		PrintWriter fw = new PrintWriter(MAPPE + filnavn);
+		
+		for (int i = 0; i < sm.length; i++) {
+			a = sm[i];
+			b = sm[i+1];
+		
+		fw.print(an + "\n" + a.toString()+b.toString());
+			if(fw != null) {
+				skrevet = true;
+		
+				fw.close();
+		}
+		}
+		//throw new UnsupportedOperationException(TODO.method());
+		}catch (Exception e) {
+			System.out.println("Feil?: " + e.getClass());
 
-		throw new UnsupportedOperationException(TODO.method());
+		}
+		return skrevet;
+		 
 	}
 }
